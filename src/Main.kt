113ac46e01ce46ac61c32b5218e53b69c96dafcc
aluochvivian <aluochviviann@gmail.com>
2022-02-20 23:54:50 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+fun main(){
+    var name = "Esther"
+    println("Hello"+ " " + name)
+    var x= 12.5
+    var y = 3.5
+    var modulus = x%y
+    println(modulus)
+    var a = 1
+    var b =2
+    var c = 3
+    var d =4
+    var total = a+b+c+d
+    println(total)
+    var intrest= "humble"
+     println( "I am " + " " +intrest)
+}
+fun name(name:String):String{
+return  name
+}
+
+fun modulus(modulus:Int):Int{
+    return modulus
+}
+fun total(total:Int):Int{
+   return  total
+}
+fun intrest( intrest:String):String{
+    return intrest
+}
